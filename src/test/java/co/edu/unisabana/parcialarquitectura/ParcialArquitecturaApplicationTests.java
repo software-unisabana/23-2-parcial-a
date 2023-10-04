4d9e01458b9ec902cae94cb6611fe3143f5121dd
David-Galeano <davidgaleanom8888@gmail.com>
2023-10-04 17:43:30 -0500

    Add CI.yml and modify application properties

--- src/test/java/co/edu/unisabana/parcialarquitectura/ParcialArquitecturaApplicationTests.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/ParcialArquitecturaApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.unisabana.parcialarquitectura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class ParcialArquitecturaApplicationTests {
 
 	@Test
