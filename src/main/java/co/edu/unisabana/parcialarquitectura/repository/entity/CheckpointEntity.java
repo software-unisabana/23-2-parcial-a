5d4fa5deef1e1d9ef7525710057642137e19ad13
Juanes Pinilla <juanpinpin@unisabana.edu.co>
2023-10-04 17:34:12 -0500

    Invierte las dependencias entre Service y Repository

--- src/main/java/co/edu/unisabana/parcialarquitectura/repository/entity/CheckpointEntity.java
+++ src/main/java/co/edu/unisabana/parcialarquitectura/repository/entity/CheckpointEntity.java
@@ -2,15 +2,14 @@ package co.edu.unisabana.parcialarquitectura.repository.entity;
 
 import co.edu.unisabana.parcialarquitectura.service.model.Checkin;
 import java.time.LocalDateTime;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Data
 @Table(name = "CHECKPOINT")
 @AllArgsConstructor
@@ -20,24 +19,59 @@ public class CheckpointEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column
   private Integer id;
 
+  @Column
   private String type;
 
+  @Column
   private String facility;
 
+  @Column
   private String driver;
 
+  @Column
   private int dayOfMonth;
 
+  @Column
   private LocalDateTime creationDate;
 
+  @Column
   private boolean finalized;
 
   public Checkin toCheckin() {
     Checkin checkin = new Checkin(this.facility, this.driver, this.dayOfMonth);
     checkin.setId(this.id);
     return checkin;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setFacility(String facility) {
+    this.facility = facility;
+  }
+
+  public void setDriver(String driver) {
+    this.driver = driver;
+  }
+
+  public void setDayOfMonth(int dayOfMonth) {
+    this.dayOfMonth = dayOfMonth;
+  }
+
+  public void setCreationDate(LocalDateTime creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public void setFinalized(boolean finalized) {
+    this.finalized = finalized;
   }
 
   public static CheckpointEntity fromCheckin(Checkin checkin) {
